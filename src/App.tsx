a5f2258e8233cb37d18531caa44b4e210059ae17
Priyesh kekan <priyeshkekan3@gmail.com>
2025-01-03 11:20:24 +0530

    update file names and directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from './components/Header';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Events from './components/Events';
 import Team from './components/Team';
-import Social from './components/Footer';
+import Social from './components/Social';
 
 function App() {
   return (
